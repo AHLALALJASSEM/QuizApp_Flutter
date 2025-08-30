c02696b8c744be1ac465ba79cc4b93a5ca122ee2
Ahlal Aljasem <ahlalaljasem@gmail.com>
2025-08-30 08:37:15 +0300

    add package bloc and http  and package to check net Status

--- lib/presentation/screens/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:quizapp_flutter/core/constants/assets.dart';
-import 'package:quizapp_flutter/core/constants/colors.dart';
 import 'package:quizapp_flutter/routes.dart';
 
 class OnboardingScreen extends StatelessWidget {
